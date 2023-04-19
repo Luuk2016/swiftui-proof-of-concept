df17c4cef16f5af6c4a06219fed4d372b43db4fd
Luuk Kenselaar <luuk@lkenselaar.dev>
2023-04-19 16:22:47 +0200

    First files

---
+++ SwiftUIProofOfConcept/SwiftUIProofOfConceptApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIProofOfConceptApp.swift
+//  SwiftUIProofOfConcept
+//
+//  Created by Luuk Kenselaar on 19/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIProofOfConceptApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
