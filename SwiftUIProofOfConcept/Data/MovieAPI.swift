97ea1893ddb7c3a0a33874f7d75ae21363dac6f0
Luuk Kenselaar <luuk@lkenselaar.dev>
2023-05-03 13:52:25 +0200

    Rewrite URL check

--- SwiftUIProofOfConcept/Data/MovieAPI.swift
+++ SwiftUIProofOfConcept/Data/MovieAPI.swift
@@ -7,16 +7,14 @@
 
 import SwiftUI
 
-final class MovieAPI {
+struct MovieAPI {
     // MARK: Properties
     private let baseURL: String = "https://api.themoviedb.org/3"
     private let apiKey: String = "f829d6f95757da6cb795e8aae1c7685b"
     
     // MARK: Methods
     func getTopRatedMovies() async throws -> [Movie] {
-        guard let url = URL(string: baseURL + "/movie/top_rated?api_key=" + apiKey + "&language=en-US&page=1") else {
-            throw URLError(.badURL)
-        }
+        let url: String = baseURL + "/movie/top_rated?api_key=" + apiKey + "&language=en-US&page=1"
         
         let response = try await execute(type: MovieAPIResponse.self, url: url)
         
@@ -24,24 +22,27 @@ final class MovieAPI {
     }
     
     func getTrendingMovies() async throws -> [Movie] {
-        guard let url = URL(string: baseURL + "/trending/movie/day?api_key=" + apiKey + "&language=en-US&page=1") else {
-            throw URLError(.badURL)
-        }
+        let url: String = baseURL + "/trending/movie/day?api_key=" + apiKey + "&language=en-US&page=1"
         
         let response = try await execute(type: MovieAPIResponse.self, url: url)
         
         return response.results
     }
     
-    
-    private func execute<T: Decodable>(type: T.Type, url: URL) async throws -> T {
+    private func execute<T: Decodable>(type: T.Type, url: String) async throws -> T {
+        guard let url = URL(string: url) else {
+            throw URLError(.badURL)
+        }
+        
         let (data, response) = try await URLSession.shared.data(from: url)
         
         guard let httpResponse = response as? HTTPURLResponse, (200..<400).contains(httpResponse.statusCode) else {
             throw URLError(.badServerResponse)
         }
-
-        let result = try JSONDecoder().decode(T.self, from: data)
+        
+        guard let result = try? JSONDecoder().decode(T.self, from: data) else {
+            throw URLError(.cannotDecodeRawData)
+        }
         
         return result
     }
